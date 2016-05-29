6dfa06f9efba04c13f9ca09d02e9b3f821ce2a67
Chris Alfano <chris@jarv.us>
2016-05-29 16:16:14 -0400

    Add script to setup system-level frameworks and builds repositories

---
+++ setup-system-repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo mkdir /emergence/services/sencha
+
+sudo git clone --bare https://github.com/JarvusInnovations/extjs.git /emergence/services/sencha/frameworks-repo.git
+sudo chown root:www-data -R /emergence/services/sencha/frameworks-repo.git
+sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/frameworks-repo.git
+
+sudo git init --bare /emergence/services/sencha/builds-repo.git
+sudo chown root:www-data -R /emergence/services/sencha/builds-repo.git
+sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/builds-repo.git
+echo "/emergence/services/sencha/frameworks-repo.git/objects" > /emergence/services/sencha/builds-repo.git/objects/info/alternates
