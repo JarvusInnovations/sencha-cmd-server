23d3defd4f234f4520d5a5c72fb8d11d9eb5d6cd
Chris Alfano <chris@jarv.us>
2016-05-29 16:25:04 -0400

    Write changes before setting permissions

--- setup-system-repos.sh
+++ setup-system-repos.sh
@@ -6,6 +6,6 @@ sudo chown root:www-data -R /emergence/services/sencha/frameworks-repo.git
 sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/frameworks-repo.git
 
 sudo git init --bare /emergence/services/sencha/builds-repo.git
+echo "/emergence/services/sencha/frameworks-repo.git/objects" > /emergence/services/sencha/builds-repo.git/objects/info/alternates
 sudo chown root:www-data -R /emergence/services/sencha/builds-repo.git
 sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/builds-repo.git
-echo "/emergence/services/sencha/frameworks-repo.git/objects" > /emergence/services/sencha/builds-repo.git/objects/info/alternates
