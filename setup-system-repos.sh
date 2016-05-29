c418fdc227bcae4147996f15e1afd9f94507db33
Chris Alfano <chris@jarv.us>
2016-05-29 17:09:01 -0400

    Use `sudo tee` to write to alternates file

--- setup-system-repos.sh
+++ setup-system-repos.sh
@@ -6,6 +6,6 @@ sudo chown root:www-data -R /emergence/services/sencha/frameworks.git
 sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/frameworks.git
 
 sudo git init --bare /emergence/services/sencha/builds.git
-echo "/emergence/services/sencha/frameworks.git/objects" > /emergence/services/sencha/builds.git/objects/info/alternates
+echo "/emergence/services/sencha/frameworks.git/objects" | sudo tee /emergence/services/sencha/builds.git/objects/info/alternates
 sudo chown root:www-data -R /emergence/services/sencha/builds.git
 sudo chmod ug=Xrw,o=Xr -R /emergence/services/sencha/builds.git
