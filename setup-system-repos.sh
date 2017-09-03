f744608779de612e34e0c3a74dd7097721fd8b60
Chris Alfano <chris@jarv.us>
2017-09-03 03:44:52 +0000

    Use -p for mkdir

--- setup-system-repos.sh
+++ setup-system-repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo mkdir /emergence/services/sencha
+sudo mkdir -p /emergence/services/sencha
 
 sudo git clone --bare https://github.com/JarvusInnovations/extjs.git /emergence/services/sencha/frameworks.git
 sudo chown root:www-data -R /emergence/services/sencha/frameworks.git
